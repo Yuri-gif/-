e302e020b9cd842f9c4ce2782ede1f5e2c3f5270
Yuri <urakokurin197@gmail.com>
2022-11-15 19:47:57 +0400

    Project C#

---
+++ Example2_DZ2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c=Convert.ToInt32(Console.ReadLine());
+if(b>a)
+{
+    Console.WriteLine("Максимальное число:"+b);
+}
+else if(c>a){
+    Console.WriteLine("Максимальное число:"+b);
+}
+else{
+    Console.WriteLine("Максимальное число:"+a);
+}
---
+++ Example3_DZ1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=Convert.ToInt32(Console.ReadLine());
+if(a%2==0)
+{
+    Console.WriteLine("Даннле число "+ a +" является чётным");
+}
+else{
+    Console.WriteLine("Даннле число "+ a +" не является чётным");
+}
